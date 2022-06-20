cedcd0d309a4bdcecb8d87b9037affab9e0aabf2
Miguel <miguel@gomat.co>
2022-06-20 21:04:27 +0000

    modify logic for components inside component data

--- WhatsApp/module.js
+++ WhatsApp/module.js
@@ -7,38 +7,41 @@ let _axios = null;
 const _whatsAppInstance = () => {
     _axios = axios.create({
         baseURL: 'https://graph.facebook.com/v13.0',
-    }); 
-    
+    });
+
     _axios.defaults.headers.common['Content-Type'] = 'application/json';
-    
+
     _axios.defaults.headers.common['Authorization'] = config.whatsAppToken ? `Bearer ${config.whatsAppToken}` : null;
-    
+
     return _axios;
 };
 
 const sendMessage = async (data) => {
     _axios = _whatsAppInstance();
-    
+
     const {
         template,
         language,
-        personalizedMessage,
+        to,
+        components,
     } = data;
 
-    const response = await _axios.post(`/${config.whatsAppPhoneIdentifier}/messages`, { 
-        messaging_product: "whatsapp", 
-        to: "50762647737", 
-        type: template ? "template" : null, // to identify, we will use templates only for now 
-        template: 
-            { 
-                name: template, 
-                language: 
-                    { 
-                        code: language || "en_US", 
-                    } 
-                } 
-            }
-    );
+    if (!template) {
+        throw new Error('403:only_template_allowed_for_now');
+    }
+
+    const response = await _axios.post(`/${config.whatsAppPhoneIdentifier}/messages`, {
+        messaging_product: "whatsapp",
+        to,
+        type: template ? "template" : null, // we will use templates only for now 
+        template: {
+            name: template,
+            language: {
+                code: language || "en_US",
+            },
+            "components": components,
+        },
+    }, );
 
     return response ? response.data : null;
 };
